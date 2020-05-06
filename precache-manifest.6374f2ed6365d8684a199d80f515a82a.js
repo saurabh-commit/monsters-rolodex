7f70d638cd4603b3b91c688a21f1706927e74519
Saurabh <saurabhshukla19011993@gmail.com>
2020-05-06 18:50:58 +0530

    Updates

---
+++ precache-manifest.6374f2ed6365d8684a199d80f515a82a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3695972c2b9fd40a3350f5a85e61c15f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "324d05d4d613af7120e7",
+    "url": "/monsters-rolodex/static/css/main.3541833c.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "324d05d4d613af7120e7",
+    "url": "/monsters-rolodex/static/js/main.fe7d229f.chunk.js"
+  },
+  {
+    "revision": "e3f9453037e5f28f8167",
+    "url": "/monsters-rolodex/static/js/runtime-main.7c0f1f0f.js"
+  }
+]);
